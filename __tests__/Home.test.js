b7d80215f09941a78effb15f300c59af1602d549
mohsinchd <mohsinshoaib735@gmail.com>
2025-06-14 20:56:10 +0500

    fixed test issue

--- __tests__/Home.test.js
+++ __tests__/Home.test.js
@@ -2,35 +2,11 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Home from "../app/page";
 
-// Mock the Image component from next/image
-jest.mock("next/image", () => ({
-  __esModule: true,
-  default: ({ src, alt, priority, ...props }) => {
-    return (
-      <img
-        src={src}
-        alt={alt}
-        data-priority={priority ? "true" : undefined}
-        {...props}
-      />
-    );
-  },
-}));
-
 describe("Home Page", () => {
-  it("renders Next.js logo", () => {
+  it("contains 'Hello world' text", () => {
     render(<Home />);
 
-    // Check if the Next.js logo is in the document
-    const logoImage = screen.getByAltText("Next.js logo");
-    expect(logoImage).toBeInTheDocument();
-    expect(logoImage).toHaveAttribute("src", "/next.svg");
-  });
-
-  it("contains 'Get started by editing' text", () => {
-    render(<Home />);
-
-    const startedText = screen.getByText(/Get started by editing/i);
+    const startedText = screen.getByText(/Hello world/i);
     expect(startedText).toBeInTheDocument();
   });
 });
